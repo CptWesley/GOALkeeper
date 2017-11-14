1bc6184b0de1358306dd882b5b3f88ecc6dcc35b
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-11-14 10:31:15 +0100

    Add basic project set up

---
+++ src/main/java/nl/tudelft/goalanalyzer/Program.java
@@ -0,0 +1,20 @@
+package nl.tudelft.goalanalyzer;
+
+/**
+ * Main class of the program.
+ */
+public final class Program {
+
+    /**
+     * Prevent instantiation.
+     */
+    private Program() { }
+
+    /**
+     * Entry point of the program.
+     * @param args Launch arguments.
+     */
+    public static void main(String[] args) {
+        // TODO: Add logic.
+    }
+}

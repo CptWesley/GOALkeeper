f18c4d6af223e2b398932c136810db08debdb2dc
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-11-18 16:42:27 +0100

    Add json verifier util class

---
+++ src/main/java/nl/tudelft/goalanalyzer/util/VerifiedJsonParser.java
@@ -0,0 +1,79 @@
+package nl.tudelft.goalanalyzer.util;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import nl.tudelft.goalanalyzer.exceptions.MalformedRulesException;
+
+/**
+ * Wrapped Gson parsing.
+ */
+final class VerifiedJsonParser {
+
+    /**
+     * Prevents instantiation.
+     */
+    private VerifiedJsonParser() { }
+
+    /**
+     * Gets the integer value of child the child with name from the given element.
+     * @param object Object to get the child from.
+     * @param name Name of the child.
+     * @return Integer value at the child.
+     * @throws MalformedRulesException Thrown when the json is not formatted properly.
+     */
+    static int getInt(JsonObject object, String name)
+            throws MalformedRulesException {
+        JsonElement element = getElement(object, name);
+        try {
+            return Integer.parseInt(element.toString());
+        } catch (NumberFormatException e) {
+            throw new MalformedRulesException("Invalid '" + name + "' type.");
+        }
+    }
+
+    /**
+     * Gets the boolean value of child the child with name from the given element.
+     * @param object Object to get the child from.
+     * @param name Name of the child.
+     * @return Integer value at the child.
+     * @throws MalformedRulesException Thrown when the json is not formatted properly.
+     */
+    static boolean getBoolean(JsonObject object, String name)
+            throws MalformedRulesException {
+        JsonElement element = getElement(object, name);
+        try {
+            return Boolean.parseBoolean(element.toString());
+        } catch (NumberFormatException e) {
+            throw new MalformedRulesException("Invalid '" + name + "' type.");
+        }
+    }
+
+    /**
+     * Gets the string value of child the child with name from the given element.
+     * @param object Object to get the child from.
+     * @param name Name of the child.
+     * @return Integer value at the child.
+     * @throws MalformedRulesException Thrown when the json is not formatted properly.
+     */
+    static String getString(JsonObject object, String name)
+            throws MalformedRulesException {
+        JsonElement element = getElement(object, name);
+        return element.toString();
+    }
+
+    /**
+     * Gets the child element of an object.
+     * @param object Object to get the child from.
+     * @param name Name of the child.
+     * @return The child element.
+     * @throws MalformedRulesException Thrown when child can not be found.
+     */
+    private static JsonElement getElement(JsonObject object, String name)
+            throws MalformedRulesException {
+        JsonElement element = object.get(name);
+        if (element == null) {
+            throw new MalformedRulesException("Missing '" + name + "' setting.");
+        }
+        return element;
+    }
+}

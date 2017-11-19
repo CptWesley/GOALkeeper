43dbbeb045d8bcc10fbe0f3566ed9e480dd60485
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-11-19 21:11:50 +0100

    Add lines of code checker

--- src/main/java/nl/tudelft/goalanalyzer/checking/checkers/LinesOfCodeChecker.java
+++ src/main/java/nl/tudelft/goalanalyzer/checking/checkers/LinesOfCodeChecker.java
@@ -2,10 +2,15 @@ package nl.tudelft.goalanalyzer.checking.checkers;
 
 import nl.tudelft.goalanalyzer.checking.Checker;
 import nl.tudelft.goalanalyzer.checking.violations.Violation;
+import nl.tudelft.goalanalyzer.rules.Rule;
 import nl.tudelft.goalanalyzer.rules.RuleSet;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Class that checks the length of files.
@@ -13,8 +18,63 @@ import java.util.Collection;
 @Checker
 public final class LinesOfCodeChecker implements CheckerInterface {
 
+    private static final String RULE_NAME = "LOC";
+    private static final String VIOLATION_NAME = "Too Many Lines Of Code";
+
     @Override
     public Collection<Violation> run(String[] files, RuleSet ruleSet) {
-        return new ArrayList<>();
+        List<Violation> violations = new ArrayList<>();
+        if (!isEnabled(ruleSet)) {
+            return violations;
+        }
+        Rule rule = ruleSet.getRule(RULE_NAME);
+        for (String fileName : files) {
+            int lines;
+            try {
+                lines = countLines(fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+                continue;
+            }
+            int severity = rule.severityOf(lines);
+            if (severity >= ruleSet.getErrorSeverity()) {
+                violations.add(new Violation(VIOLATION_NAME, severity)
+                        .setActualValue(lines)
+                        .setMaximumValue(rule.maxValueBefore(ruleSet.getErrorSeverity()))
+                        .setFile(fileName));
+            }
+        }
+        return violations;
+    }
+
+    /**
+     * Checks if this checker should be enabled.
+     * @param ruleSet RuleSet to check for.
+     * @return True if enabled, false otherwise.
+     */
+    private boolean isEnabled(RuleSet ruleSet) {
+        if (!ruleSet.hasRule(RULE_NAME)) {
+            return false;
+        }
+        Rule rule = ruleSet.getRule(RULE_NAME);
+        if (!rule.isEnabled()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Count the number of lines in a file.
+     * @param fileName File to check for.
+     * @return Number of lines.
+     */
+    private int countLines(String fileName) throws IOException {
+        BufferedReader reader  = new BufferedReader(new FileReader(fileName));
+        int lines = 0;
+        while (reader.readLine() != null) {
+            ++lines;
+        }
+        reader.close();
+        return lines;
     }
 }

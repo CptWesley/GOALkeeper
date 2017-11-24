42d144b2907f2975ef93a106fcd5999c294cc435
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-11-24 14:34:38 +0100

    Add ConsoleColor enum

---
+++ src/main/java/nl/tudelft/goalanalyzer/util/console/ConsoleColor.java
@@ -0,0 +1,28 @@
+package nl.tudelft.goalanalyzer.util.console;
+
+import lombok.Getter;
+
+/**
+ * Enumeration of console colors.
+ */
+public enum ConsoleColor {
+    DEFAULT("\u001B[0m"),
+    BLACK("\u001B[30m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    BLUE("\u001B[34m"),
+    PURPLE("\u001B[35m"),
+    CYAN("\u001B[36m"),
+    WHITE("\u001B[37m");
+
+    @Getter private String ansi;
+
+    /**
+     * Initializes a new ConsoleColor enum.
+     * @param ansi ANSI escape character set.
+     */
+    ConsoleColor(final String ansi) {
+        this.ansi = ansi;
+    }
+}

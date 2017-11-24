00470789abb102f899ca10de99869dd58946e76f
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-11-24 15:08:07 +0100

    Add exit code container class

---
+++ src/main/java/nl/tudelft/goalanalyzer/ExitCode.java
@@ -0,0 +1,16 @@
+package nl.tudelft.goalanalyzer;
+
+/**
+ * Class containing exit codes.
+ */
+public final class ExitCode {
+
+    public static final int NO_RULES = 1;
+    public static final int NO_MAS = 2;
+    public static final int ERROR_FOUND = 3;
+
+    /**
+     * Prevents instantiation.
+     */
+    private ExitCode() { }
+}

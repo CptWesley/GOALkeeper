f3a4a7ef48b1d5298632bc98e3fb609207dd8bf7
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-11-24 17:47:02 +0100

    Add successful exit code

--- src/main/java/nl/tudelft/goalanalyzer/ExitCode.java
+++ src/main/java/nl/tudelft/goalanalyzer/ExitCode.java
@@ -5,6 +5,7 @@ package nl.tudelft.goalanalyzer;
  */
 public final class ExitCode {
 
+    public static final int SUCCESSFUL = 0;
     public static final int NO_RULES = 1;
     public static final int NO_MAS = 2;
     public static final int ERROR_FOUND = 3;

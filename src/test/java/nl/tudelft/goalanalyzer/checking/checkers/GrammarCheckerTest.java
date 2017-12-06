36de2b26d61a045b0d6c2395b9be923b2375a531
Cedric <cedric.willekens@hotmail.com>
2017-12-06 20:17:57 +0100

    Add test for positive case

--- src/test/java/nl/tudelft/goalanalyzer/checking/checkers/GrammarCheckerTest.java
+++ src/test/java/nl/tudelft/goalanalyzer/checking/checkers/GrammarCheckerTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import java.io.IOException;
 import java.util.Collection;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 /**
  * Created by Cedric Willekens (4530373) on 12/6/2017.
  */
@@ -21,10 +23,18 @@ public class GrammarCheckerTest {
     }
 
     @Test
-    void runtest() throws IOException, MalformedRulesException {
+    void runtestFail() throws IOException, MalformedRulesException {
         Collection<Violation> violations = checker.run(new String[]{
                 "src/test/resources/testfiles/failed.mas2g"
         }, RuleSet.load("src/test/resources/testfiles/checker-test-rules.json"));
-        System.out.println(violations.toString());
+        assertThat(violations.size()).isGreaterThan(0);
+    }
+
+    @Test
+    void runTestSucceed() throws IOException, MalformedRulesException {
+        Collection<Violation> violations = checker.run(new String[]{
+                "src/test/resources/testfiles/bw4t-working/bw4t.mas2g"
+        }, RuleSet.load("src/test/resources/testfiles/checker-test-rules.json"));
+        assertThat(violations.size()).isEqualTo(0);
     }
 }

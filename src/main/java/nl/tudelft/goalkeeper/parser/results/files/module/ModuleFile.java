b30b1c1bc7443b341f6ed691c3bf42d0dbf26fdd
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-12-27 12:42:32 +0100

    Add module file class

---
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/module/ModuleFile.java
@@ -0,0 +1,45 @@
+package nl.tudelft.goalkeeper.parser.results.files.module;
+
+import nl.tudelft.goalkeeper.parser.results.files.File;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Class which holds data on a module file.
+ */
+public class ModuleFile extends File {
+
+    private List<Rule> rules;
+
+    /**
+     * Creates a new ModuleFile instance.
+     * @param fileName File path to parse.
+     * @throws IOException Thrown when file could not be read.
+     */
+    public ModuleFile(String fileName) throws IOException {
+        super(fileName);
+        this.rules = new ArrayList<>();
+    }
+
+    /**
+     * Get a list of all rules in the module.
+     * @return List of all rules in the module.
+     */
+    public List<Rule> getRules() {
+        return Collections.unmodifiableList(rules);
+    }
+
+    /**
+     * Adds a rule to the module.
+     * @param rule Rule to be added.
+     * @return Current object.
+     */
+    public ModuleFile addRule(Rule rule) {
+        rules.add(rule);
+        return this;
+    }
+
+}
---
+++ src/test/java/nl/tudelft/goalkeeper/parser/results/files/module/ModuleFileTest.java
@@ -0,0 +1,37 @@
+package nl.tudelft.goalkeeper.parser.results.files.module;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class for the ModuleFile class.
+ */
+class ModuleFileTest {
+
+    private ModuleFile file;
+
+    /**
+     * Sets up the testing environment before each test.
+     */
+    @BeforeEach
+    void setup() throws IOException {
+        file = new ModuleFile("src/test/resources/testfiles/emptyfile.txt");
+    }
+
+    /**
+     * Checks that rule adding works properly.
+     */
+    @Test
+    void getRulesTest() {
+        Rule rule = Mockito.mock(Rule.class);
+        assertThat(file.getRules()).isEmpty();
+        assertThat(file.addRule(rule)).isSameAs(file);
+        assertThat(file.getRules()).hasSize(1);
+        assertThat(file.getRules().get(0)).isSameAs(rule);
+    }
+}

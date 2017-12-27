e6361c020df93c5c1b6fcd2ab6ee8649c370d01b
Cedric <cedric.willekens@hotmail.com>
2017-12-27 17:10:37 -0300

    Fix import issues

--- src/main/java/nl/tudelft/goalkeeper/checking/checkers/TestChecker.java
+++ src/main/java/nl/tudelft/goalkeeper/checking/checkers/TestChecker.java
@@ -1,10 +1,10 @@
 package nl.tudelft.goalkeeper.checking.checkers;
 
-import nl.tudelft.goalanalyzer.checking.Checker;
-import nl.tudelft.goalanalyzer.checking.violations.Violation;
-import nl.tudelft.goalanalyzer.rules.Rule;
-import nl.tudelft.goalanalyzer.rules.RuleSet;
-import nl.tudelft.goalanalyzer.util.FileParser;
+import nl.tudelft.goalkeeper.checking.Checker;
+import nl.tudelft.goalkeeper.checking.violations.Violation;
+import nl.tudelft.goalkeeper.rules.Rule;
+import nl.tudelft.goalkeeper.rules.RuleSet;
+import nl.tudelft.goalkeeper.util.FileParser;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,6 +16,17 @@ public class TestChecker implements CheckerInterface {
 
     private static final String RULE_NAME = "TEST";
     private static final String VIOLATION_NAME = "Test file runner";
+
+    private boolean isEnabled(RuleSet ruleSet) {
+        if (!ruleSet.hasRule(RULE_NAME)) {
+            return false;
+        }
+        Rule rule = ruleSet.getRule(RULE_NAME);
+        if (!rule.isEnabled()) {
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public Collection<Violation> run(String[] files, RuleSet ruleSet) {
@@ -31,18 +42,6 @@ public class TestChecker implements CheckerInterface {
             //Run goalchecke
             // and parser the percentages to violations.
         }
-
         return violations;
-    }
-
-    private boolean isEnabled(RuleSet ruleSet) {
-        if (!ruleSet.hasRule(RULE_NAME)) {
-            return false;
-        }
-        Rule rule = ruleSet.getRule(RULE_NAME);
-        if (!rule.isEnabled()) {
-            return false;
-        }
-        return true;
     }
 }

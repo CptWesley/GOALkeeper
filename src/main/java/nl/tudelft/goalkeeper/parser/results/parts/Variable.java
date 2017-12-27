f5cae94226577091afe55d2e787ea44ca88f4e64
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-12-27 10:26:13 +0100

    Add abstract getIdentifier method

--- src/main/java/nl/tudelft/goalkeeper/parser/results/parts/Variable.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/parts/Variable.java
@@ -4,4 +4,10 @@ package nl.tudelft.goalkeeper.parser.results.parts;
  * Interface for all variable types.
  */
 public interface Variable {
+
+    /**
+     * Gets the identifier of this variable.
+     * @return Identifier of this variable.
+     */
+    String getIdentifier();
 }

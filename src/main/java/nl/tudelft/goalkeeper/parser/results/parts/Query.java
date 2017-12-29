7d2e79e5b5e59569f33f6e001d957b540bfb634e
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-12-29 19:01:33 +0100

    Add query overrides

--- src/main/java/nl/tudelft/goalkeeper/parser/results/parts/Query.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/parts/Query.java
@@ -11,6 +11,8 @@ import java.util.List;
  */
 public final class Query implements Expression {
 
+    private static final int HASH_MODIFIER = 43;
+
     @Getter private String identifier;
     private List<Expression> parts;
 
@@ -39,5 +41,57 @@ public final class Query implements Expression {
      */
     public List<Expression> getParts() {
         return Collections.unmodifiableList(parts);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Query) {
+            Query that = (Query) o;
+            if (!this.identifier.equals(that.identifier)) {
+                return false;
+            }
+            if (that.parts.size() != this.parts.size()) {
+                return false;
+            }
+            for (int i = 0; i < this.parts.size(); ++i) {
+                if (!that.parts.get(i).equals(this.parts.get(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        int result = getIdentifier().hashCode() ^ HASH_MODIFIER;
+        for (int i = 0; i < parts.size(); ++i) {
+            result += parts.get(i).hashCode() ^ (i + 1);
+        }
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(identifier).append('(');
+        for (int i = 0; i < parts.size(); ++i) {
+            sb.append(parts.get(i));
+            if (i < parts.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append(')');
+        return sb.toString();
     }
 }
--- src/test/java/nl/tudelft/goalkeeper/parser/results/parts/QueryTest.java
+++ src/test/java/nl/tudelft/goalkeeper/parser/results/parts/QueryTest.java
@@ -14,6 +14,7 @@ class QueryTest {
     private static final String identifier = "DGFDS";
 
     private Query query;
+    private Expression p1, p2;
 
     /**
      * Sets up the testing environment before each test.
@@ -21,6 +22,10 @@ class QueryTest {
     @BeforeEach
     void setup() {
         query = new Query(identifier);
+        p1 = Mockito.mock(Expression.class);
+        p2 = Mockito.mock(Expression.class);
+        Mockito.when(p1.toString()).thenReturn("bla1");
+        Mockito.when(p2.toString()).thenReturn("bla2");
     }
 
     /**
@@ -41,5 +46,81 @@ class QueryTest {
         assertThat(query.addPart(part)).isSameAs(query);
         assertThat(query.getParts()).hasSize(1);
         assertThat(query.getParts()).containsExactly(part);
+    }
+
+    /**
+     * Checks that the toString override is correct.
+     */
+    @Test
+    void toStringTest() {
+        assertThat(query.toString()).isEqualTo(identifier + "()");
+        assertThat(query.addPart(p1).toString()).isEqualTo(identifier + "(bla1)");
+        assertThat(query.addPart(p2).toString()).isEqualTo(identifier + "(bla1, bla2)");
+    }
+
+    /**
+     * Checks that two equal objects have the same hashcode.
+     */
+    @Test
+    void hashCodeTest() {
+        Query other = new Query(identifier);
+        assertThat(query.hashCode()).isEqualTo(other.hashCode());
+    }
+
+    /**
+     * Checks that an object is considered equal to itself.
+     */
+    @Test
+    void equalSameObject() {
+        assertThat(query).isEqualTo(query);
+    }
+
+    /**
+     * Checks that two equal objects are considered equal.
+     */
+    @Test
+    void equalSameIdentifier() {
+        assertThat(query).isEqualTo(new Query(identifier));
+    }
+
+    /**
+     * Checks that two equal objects are considered equal.
+     */
+    @Test
+    void equalSameParts() {
+        assertThat(query.addPart(p1)).isEqualTo(new Query(identifier).addPart(p1));
+    }
+
+    /**
+     * Checks that objects with different identifiers are not considered equal.
+     */
+    @Test
+    void notEqualDifferentIdentifier() {
+        assertThat(query).isNotEqualTo(new Query("WGD"));
+    }
+
+    /**
+     * Checks that objects with different parts are not considered equal.
+     */
+    @Test
+    void notEqualDifferentParts() {
+        assertThat(query).isNotEqualTo(new Query(identifier).addPart(p2));
+        assertThat(query.addPart(p1)).isNotEqualTo(new Query(identifier).addPart(p2));
+    }
+
+    /**
+     * Checks that objects with different types are not considered equal.
+     */
+    @Test
+    void notEqualDifferentType() {
+        assertThat(query).isNotEqualTo(new Constant(identifier));
+    }
+
+    /**
+     * Checks that an object is not considered to be equal to null.
+     */
+    @Test
+    void notEqualNull() {
+        assertThat(query).isNotEqualTo(null);
     }
 }

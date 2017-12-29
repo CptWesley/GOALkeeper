ae50d883ac1e4b4f5f1bf34c0e2a8ac065e2a3b1
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-12-29 18:23:45 +0100

    Add equals and hashcodes

--- src/main/java/nl/tudelft/goalkeeper/parser/results/parts/Variable.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/parts/Variable.java
@@ -5,11 +5,33 @@ package nl.tudelft.goalkeeper.parser.results.parts;
  */
 public class Variable extends Parameter {
 
+    private static final int HASH_MODIFIER = 21;
+
     /**
      * Creates a new free variable instance.
      * @param identifier Identifier of the variable.
      */
     public Variable(String identifier) {
         super(identifier);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Variable) {
+            Variable that = (Variable) o;
+            return this.getIdentifier().equals(that.getIdentifier());
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return getIdentifier().hashCode() ^ HASH_MODIFIER;
     }
 }

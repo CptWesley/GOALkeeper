f3d6cde833c2632502d4d72817ba777a1dec82e7
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-12-30 19:08:45 +0100

    Add prefix and suffix to rule type

--- src/main/java/nl/tudelft/goalkeeper/parser/results/files/module/RuleType.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/module/RuleType.java
@@ -1,8 +1,24 @@
 package nl.tudelft.goalkeeper.parser.results.files.module;
 
+import lombok.Getter;
+
 /**
  * Enumeration of rule types.
  */
 public enum RuleType {
-    IF, FORALL, LISTALL
+    IF("if", "then"),
+    FORALL("forall", "do"),
+    LISTALL("listall", "do");
+
+    @Getter private String prefix, suffix;
+
+    /**
+     * Creates an instance of the RuleType enum.
+     * @param prefix Condition prefix of the rule.
+     * @param suffix Condition suffix of the rule.
+     */
+    RuleType(String prefix, String suffix) {
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
 }

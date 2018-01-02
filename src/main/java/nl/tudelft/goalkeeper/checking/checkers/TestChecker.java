4c834497e7226046314c37bf2cef49537b650237
Cedric <cedric.willekens@hotmail.com>
2018-01-02 09:24:18 -0300

    Create initial run setup

--- src/main/java/nl/tudelft/goalkeeper/checking/checkers/TestChecker.java
+++ src/main/java/nl/tudelft/goalkeeper/checking/checkers/TestChecker.java
@@ -1,5 +1,10 @@
 package nl.tudelft.goalkeeper.checking.checkers;
 
+import goal.tools.TestRun;
+import goal.tools.errorhandling.exceptions.GOALRunFailedException;
+import languageTools.analyzer.FileRegistry;
+import languageTools.analyzer.test.TestValidator;
+import languageTools.program.test.TestProgram;
 import nl.tudelft.goalkeeper.checking.Checker;
 import nl.tudelft.goalkeeper.checking.violations.Violation;
 import nl.tudelft.goalkeeper.rules.Rule;
@@ -7,6 +12,7 @@ import nl.tudelft.goalkeeper.rules.RuleSet;
 import nl.tudelft.goalkeeper.util.FileParser;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -39,6 +45,18 @@ public class TestChecker implements CheckerInterface {
         Rule rule = ruleSet.getRule(RULE_NAME);
         List<File> testFiles = FileParser.getTestFiles(files);
         for (File file: testFiles) {
+            try {
+                FileRegistry fileRegistry = new FileRegistry();
+                TestValidator test = new TestValidator(file.getCanonicalPath(), fileRegistry);
+                TestProgram program = test.getProgram();
+                TestRun run = new TestRun(program, false);
+                run.setDebuggerOutput(false);
+                run.run(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (GOALRunFailedException e) {
+                e.printStackTrace();
+            }
             //Run goalchecke
             // and parser the percentages to violations.
         }

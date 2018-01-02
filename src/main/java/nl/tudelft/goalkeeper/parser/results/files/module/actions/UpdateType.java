9ce09e971d11a9951fd7e078bb3d4a279c1af739
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-01-02 17:13:54 +0100

    Add update types

---
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/module/actions/UpdateType.java
@@ -0,0 +1,23 @@
+package nl.tudelft.goalkeeper.parser.results.files.module.actions;
+
+import lombok.Getter;
+
+/**
+ * Enumeration of update types.
+ */
+public enum UpdateType {
+    INSERT("insert/1"),
+    DELETE("delete/1"),
+    ADOPT("adopt/1"),
+    DROP("drop/1");
+
+    @Getter private String identifier;
+
+    /**
+     * Defines what kind of update action we perform.
+     * @param identifier Identifier of the action.
+     */
+    UpdateType(String identifier) {
+        this.identifier = identifier;
+    }
+}

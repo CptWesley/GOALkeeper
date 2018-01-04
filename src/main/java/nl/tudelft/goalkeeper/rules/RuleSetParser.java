ab4002fadc4b4356b1e61ffb46f4a119def6b1cf
Cedric <cedric.willekens@hotmail.com>
2018-01-04 11:06:09 -0300

    Remove jar adding during parsing

--- src/main/java/nl/tudelft/goalkeeper/rules/RuleSetParser.java
+++ src/main/java/nl/tudelft/goalkeeper/rules/RuleSetParser.java
@@ -119,9 +119,6 @@ class RuleSetParser {
         try {
             stage.setMax(VerifiedJsonParser.getInteger(object, "max"));
         } catch (MalformedRulesException ignored) { /* Keep default values. */ }
-        try {
-            stage.setJar(VerifiedJsonParser.getString(object, "jar"));
-        } catch (MalformedRulesException ignored) { /* Keep defualt values */ }
         return stage;
     }
 }

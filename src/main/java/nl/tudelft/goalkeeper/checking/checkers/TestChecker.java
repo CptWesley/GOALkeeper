aaefd8fff3d47589fb654b4d9c971146804f98b1
Cedric <cedric.willekens@hotmail.com>
2018-01-04 11:01:11 -0300

    Create function to parse results from the run to violations

--- src/main/java/nl/tudelft/goalkeeper/checking/checkers/TestChecker.java
+++ src/main/java/nl/tudelft/goalkeeper/checking/checkers/TestChecker.java
@@ -2,11 +2,13 @@ package nl.tudelft.goalkeeper.checking.checkers;
 
 import goal.tools.TestRun;
 import goal.tools.errorhandling.exceptions.GOALRunFailedException;
+import groovy.lang.Tuple2;
 import languageTools.analyzer.FileRegistry;
 import languageTools.analyzer.test.TestValidator;
 import languageTools.program.test.TestProgram;
 import nl.tudelft.goalkeeper.checking.Checker;
 import nl.tudelft.goalkeeper.checking.violations.Violation;
+import nl.tudelft.goalkeeper.checking.violations.source.FileSource;
 import nl.tudelft.goalkeeper.rules.Rule;
 import nl.tudelft.goalkeeper.rules.RuleSet;
 import nl.tudelft.goalkeeper.util.FileParser;
@@ -42,23 +44,37 @@ public class TestChecker implements CheckerInterface {
             return violations;
         }
 
+        ArrayList<Tuple2<File, Double>> results = new ArrayList<>();
+
         Rule rule = ruleSet.getRule(RULE_NAME);
         List<File> testFiles = FileParser.getTestFiles(files);
         for (File file: testFiles) {
             try {
-                FileRegistry fileRegistry = new FileRegistry();
-                TestValidator test = new TestValidator(file.getCanonicalPath(), fileRegistry);
+                TestValidator test = new TestValidator(file.getCanonicalPath(), new FileRegistry());
                 TestProgram program = test.getProgram();
                 TestRun run = new TestRun(program, false);
-                run.setDebuggerOutput(false);
-                run.run(true);
+                // The environment must not start to run the test program.
+                run.run(false);
+                // Add the
+                results.add(new Tuple2<File, Double>(file, 0.0));
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (GOALRunFailedException e) {
                 e.printStackTrace();
             }
-            //Run goalchecke
-            // and parser the percentages to violations.
+
+            // Calculate the severity of the errors and parse the violations.
+            results.forEach((tuple) -> {
+                int severity  = rule.severityOf(tuple.getSecond());
+                if (severity > 0) {
+                    boolean error = severity >= ruleSet.getErrorSeverity();
+                    violations.add(new Violation(VIOLATION_NAME, severity)
+                    .setActualValue(tuple.getSecond())
+                    .setSource(new FileSource(tuple.getFirst().getAbsolutePath()))
+                    .setError(error));
+                }
+            });
+
         }
         return violations;
     }

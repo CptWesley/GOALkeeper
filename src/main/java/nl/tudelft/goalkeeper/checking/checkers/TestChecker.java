6dbb5a9ebef0ec5aa6ab4c1b66c946b6717f748d
Cedric <cedric.willekens@hotmail.com>
2018-01-06 21:44:52 +0100

    Cleanup the TestChecker. This will make it more maintainable for the future. There currently still is a bug in the running of goal such that the test2g file is not run correctly and thus not contain all the information about the tests.

--- src/main/java/nl/tudelft/goalkeeper/checking/checkers/TestChecker.java
+++ src/main/java/nl/tudelft/goalkeeper/checking/checkers/TestChecker.java
@@ -1,5 +1,6 @@
 package nl.tudelft.goalkeeper.checking.checkers;
 
+import goal.preferences.CorePreferences;
 import goal.tools.TestRun;
 import goal.tools.errorhandling.exceptions.GOALRunFailedException;
 import groovy.lang.Tuple2;
@@ -25,75 +26,110 @@ public class TestChecker implements CheckerInterface {
     private static final String RULE_NAME = "TEST";
     private static final String VIOLATION_NAME = "Test file runner";
 
+    /**
+     * Method to check if a rule is enabled.
+     * @param ruleSet The set of rules.
+     * @return True if the check is enabled, else false.
+     */
     private boolean isEnabled(RuleSet ruleSet) {
         if (!ruleSet.hasRule(RULE_NAME)) {
             return false;
         }
-        Rule rule = ruleSet.getRule(RULE_NAME);
-        if (!rule.isEnabled()) {
-            return false;
-        }
-        return true;
+        return ruleSet.getRule(RULE_NAME).isEnabled();
     }
 
+    /**
+     * {@inheritdoc}
+     */
     @Override
     public Collection<Violation> run(String[] files, RuleSet ruleSet) {
+        CorePreferences.setAbortOnTestFailure(false);
         List<Violation> violations = new ArrayList<>();
 
         if (!isEnabled(ruleSet)) {
             return violations;
         }
 
-        ArrayList<Tuple2<File, Double>> results = new ArrayList<>();
+        ArrayList<Tuple2<String, Double>> results = new ArrayList<>();
 
         Rule rule = ruleSet.getRule(RULE_NAME);
         List<File> testFiles = FileParser.getTestFiles(files);
-        for (File file: testFiles) {
-            try {
-                TestProgram program = setup(file.getAbsolutePath());
-                TestRun run = new TestRun(program, false);
-                // The environment must not start to run the test program.
-                run.run(true);
-                // Add the
-                results.add(new Tuple2<File, Double>(file, 50.0));
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (GOALRunFailedException e) {
-                e.printStackTrace();
-            }
-
-            // Calculate the severity of the errors and parse the violations.
-            results.forEach((tuple) -> {
-                int severity  = rule.severityOf(tuple.getSecond());
-                if (severity > 0) {
-
-//                    rule.getStages().forEach((stage -> {
-//
-//                    }));
-                    boolean error = severity >= ruleSet.getErrorSeverity();
-                    violations.add(new Violation(VIOLATION_NAME, severity)
-                    .setActualValue(tuple.getSecond())
-                    //.setMinimumValue(rule.getStages().get())
-                    .setSource(new FileSource(tuple.getFirst().getAbsolutePath()))
-                    .setError(error));
+        for (String file: files) {
+            if (FileParser.isOfType(file, "test2g")) {
+                System.out.println("Running file: " + file);
+                TestRun test = null;
+                try {
+                    test = runTest(file);
+                } catch (Exception e) {
+                    System.out.println("We were unable to run the test file: " + file);
+                    e.printStackTrace();
                 }
-            });
+                assert test != null;
+                test.cleanup();
+                // Add the result of the test run to the results list.
+                results.add(new Tuple2<>(file, 0.0));
+
+                // Calculate the severity of the errors and parse the violations.
+                results.forEach((tuple) -> {
+                    int severity = rule.severityOf(tuple.getSecond());
+                    if (severity > 0) {
+                        boolean error = severity >= ruleSet.getErrorSeverity();
+                        violations.add(new Violation(VIOLATION_NAME, severity)
+                                .setActualValue(tuple.getSecond())
+                                //.setMinimumValue(rule.getStages().get())
+                                .setSource(new FileSource(tuple.getFirst()))
+                                .setError(error));
+                    }
+                });
+            }
 
         }
         return violations;
     }
 
+    /**
+     * Method to run the test2g file.
+     * @param testFileName The test2g file to be run.
+     * @return Returns the TestRun it ran containing all the information about the tests. //TODO Make sure that all the information about the tests is actually in there...
+     * @throws GOALRunFailedException This exception is thrown when the program is unable to read
+     * the file and thus also run it.
+     */
+    private TestRun runTest(String testFileName) throws GOALRunFailedException {
+        TestProgram testProgram;
+        try {
+            testProgram = setup(testFileName);
+        } catch (IOException e) {
+            throw new GOALRunFailedException("error while reading test file " + testFileName, e);
+        }
+
+        assert testProgram != null;
+
+        TestRun testRun = new TestRun(testProgram, false);
+        testRun.run(true);
+
+        return testRun;
+    }
+
+    /**
+     * Method to setup the TestProgram so that it can be run.
+     * @param path The path to the test2g file which is to be run
+     * @return An instance of TestProgram containing the test2g file to be run.
+     * @throws Exception If the test2g file is not able to be read or does not exist
+     * an IOException will be thrown.
+     */
     private TestProgram setup(String path) throws IOException {
-        TestValidator testValidator = new TestValidator(path, new FileRegistry());
-        testValidator.validate();
-        TestProgram program = testValidator.getProgram();
+        TestValidator visitor = new TestValidator(path, new FileRegistry());
+        visitor.validate();
+        TestProgram program = visitor.getProgram();
         if (program != null && program.isValid()) {
             return program;
         } else {
-            System.out.println(testValidator.getSyntaxErrors());
-            System.out.println(testValidator.getErrors());
+            System.out.println(visitor.getSyntaxErrors());
+            System.out.println(visitor.getErrors());
             throw new IOException();
         }
     }
+
+
 
 }

36d184bc840ec7d0f6b643a3a701c2f2d582ccb0
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-02-07 11:57:01 +0100

    Remove unused imports

--- src/main/java/nl/tudelft/goalkeeper/parser/results/files/File.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/File.java
@@ -1,8 +1,6 @@
 package nl.tudelft.goalkeeper.parser.results.files;
 
 import lombok.Getter;
-import nl.tudelft.goalkeeper.checking.violations.source.Source;
-import nl.tudelft.goalkeeper.parser.results.parts.Sourceable;
 
 import java.io.IOException;
 import java.nio.file.Files;

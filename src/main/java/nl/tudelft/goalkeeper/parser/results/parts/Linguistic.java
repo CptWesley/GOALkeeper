54df99114781f44569de0e9fc32f1988526b9df8
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-02-08 11:49:08 +0100

    Add Linguistic implementation to Condition class

--- src/main/java/nl/tudelft/goalkeeper/parser/results/parts/Linguistic.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/parts/Linguistic.java
@@ -3,7 +3,7 @@ package nl.tudelft.goalkeeper.parser.results.parts;
 /**
  * Interface for classes having a KRLanguage.
  */
-interface Linguistic {
+public interface Linguistic {
     /**
      * Gets the KRLanguage of the object.
      * @return KRLanguage of the object.

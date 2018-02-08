0144eefe8725e3734d94b90d21278d08d8e2e61d
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-02-08 10:34:30 +0100

    Add exit condition types

---
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/module/details/ExitConditionType.java
@@ -0,0 +1,30 @@
+package nl.tudelft.goalkeeper.parser.results.files.module.details;
+
+import lombok.Getter;
+
+/**
+ * Exit condition types.
+ */
+public enum ExitConditionType {
+    NOGOALS("nogoals"),
+    ALWAYS("always"),
+    NEVER("never");
+
+    @Getter private String name;
+
+    /**
+     * Creates an instance of the exit condition types.
+     * @param name Name of the condition.
+     */
+    ExitConditionType(String name) {
+        this.name = name;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}

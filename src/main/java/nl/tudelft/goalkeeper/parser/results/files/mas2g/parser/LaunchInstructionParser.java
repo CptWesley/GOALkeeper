c0a371144691047ccb2200b060aa46728453fe46
Cedric <cedric.willekens@hotmail.com>
2018-02-17 19:13:31 +0100

    Create launchinstruction parser

--- src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas2g/parser/LaunchInstructionParser.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas2g/parser/LaunchInstructionParser.java
@@ -1,7 +1,18 @@
 package nl.tudelft.goalkeeper.parser.results.files.mas2g.parser;
 
+import nl.tudelft.goalkeeper.parser.results.files.mas2g.AgentDefinition.AgentDefinition;
+import nl.tudelft.goalkeeper.parser.results.files.mas2g.Launch.LaunchInstruction;
+
 /**
  * Created by Cedric Willekens (4530373) on 2/17/2018.
  */
 public class LaunchInstructionParser {
+
+    private LaunchInstructionParser() {}
+
+    public LaunchInstruction parse(languageTools.program.mas.LaunchInstruction instruction) {
+        AgentDefinition definition = AgentDefinitionParser.parse(instruction.getAgentDf());
+        return new LaunchInstruction(instruction.getAgentName()
+                , definition);
+    }
 }

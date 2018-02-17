5a2a4fe9e84f1b1a623b3ad117a970a6a6be8758
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-02-17 17:54:34 +0100

    Change ParserTest to not depend on io and goal grammar

--- src/test/java/nl/tudelft/goalkeeper/parser/ParserTest.java
+++ src/test/java/nl/tudelft/goalkeeper/parser/ParserTest.java
@@ -1,8 +1,20 @@
 package nl.tudelft.goalkeeper.parser;
 
+import languageTools.analyzer.mas.Analysis;
+import languageTools.analyzer.mas.MASValidator;
+import languageTools.errors.Message;
+import languageTools.program.agent.Module;
+import nl.tudelft.goalkeeper.checking.violations.Violation;
 import nl.tudelft.goalkeeper.parser.results.ParseResult;
+import nl.tudelft.goalkeeper.parser.results.files.module.parsers.MessageParser;
+import nl.tudelft.goalkeeper.parser.results.files.module.parsers.ModuleParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.TreeSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,27 +23,93 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class ParserTest {
 
+    private Parser parser;
+    private MASValidator validator;
+    private MessageParser messageParser;
+    private ModuleParser moduleParser;
+    private Violation violation;
+    private Message message;
+    private Analysis analysis;
+
     /**
-     * Checks that we can correctly parse failing mas files.
+     * Sets up the testing environment before each test.
+     */
+    @BeforeEach
+    void setup() {
+        parser = new Parser("");
+        validator = Mockito.mock(MASValidator.class);
+        messageParser = Mockito.mock(MessageParser.class);
+        violation = Mockito.mock(Violation.class);
+        message = Mockito.mock(Message.class);
+        moduleParser = Mockito.mock(ModuleParser.class);
+        parser.setValidator(validator);
+        parser.setMessageParser(messageParser);
+        parser.setModuleParser(moduleParser);
+        Mockito.when(messageParser.parse(message)).thenReturn(violation);
+        Mockito.when(validator.getErrors()).thenReturn(new TreeSet<>());
+        Mockito.when(validator.getSyntaxErrors()).thenReturn(new TreeSet<>());
+        Mockito.when(validator.getWarnings()).thenReturn(new TreeSet<>());
+        analysis = Mockito.mock(Analysis.class);
+        Mockito.when(validator.process()).thenReturn(analysis);
+        Mockito.when(analysis.getModuleDefinitions()).thenReturn(new HashSet<>());
+    }
+
+    /**
+     * Checks that we can correctly parse a mas file with an error.
      */
     @Test
-    void failureTest() {
-        ParseResult result = new Parser("src/test/resources/testfiles/failed.mas2g").parse();
-        assertThat(result.getViolations()).hasSize(4);
+    void errorTest() {
+        validator.getErrors().add(message);
+        ParseResult result = parser.parse();
         assertThat(result.isSuccessful()).isFalse();
-        assertThat(result.getViolations()).filteredOn(o -> o.isError()).hasSize(3);
-        assertThat(result.getViolations()).filteredOn(o -> !o.isError()).hasSize(1);
+        assertThat(result.getViolations()).hasSize(1);
+        Mockito.verify(messageParser, Mockito.times(1)).parse(message);
+        Mockito.verify(violation, Mockito.times(1)).setError(true);
+    }
+
+    /**
+     * Checks that we can correctly parse a mas file with a syntax error.
+     */
+    @Test
+    void syntaxErrorTest() {
+        validator.getSyntaxErrors().add(message);
+        ParseResult result = parser.parse();
+        assertThat(result.isSuccessful()).isFalse();
+        assertThat(result.getViolations()).hasSize(1);
+        Mockito.verify(messageParser, Mockito.times(1)).parse(message);
+        Mockito.verify(violation, Mockito.times(1)).setError(true);
+    }
+
+    /**
+     * Checks that we can correctly parse a mas file with a warning.
+     */
+    @Test
+    void warningTest() {
+        validator.getWarnings().add(message);
+        ParseResult result = parser.parse();
+        assertThat(result.isSuccessful()).isTrue();
+        assertThat(result.getViolations()).hasSize(1);
+        Mockito.verify(messageParser, Mockito.times(1)).parse(message);
+        Mockito.verify(violation, Mockito.times(1)).setError(false);
     }
 
     /**
      * Add checks for correct files.
      */
     @Test
-    void successTest() {
-        ParseResult result = new Parser("src/test/resources/testfiles/bw4t-working/bw4t.mas2g").parse();
-        assertThat(result.getViolations()).hasSize(1);
+    void successTest() throws IOException {
+        Module m1 = Mockito.mock(Module.class);
+        Module m2 = Mockito.mock(Module.class);
+        analysis.getModuleDefinitions().add(m1);
+        analysis.getModuleDefinitions().add(m2);
+
+        ParseResult result = parser.parse();
         assertThat(result.isSuccessful()).isTrue();
-        assertThat(result.getViolations()).filteredOn(o -> o.isError()).hasSize(0);
-        assertThat(result.getViolations()).filteredOn(o -> !o.isError()).hasSize(1);
+        assertThat(result.isSuccessful()).isTrue();
+        assertThat(result.getViolations()).isEmpty();
+        assertThat(result.getModules()).hasSize(2);
+
+        Mockito.verify(moduleParser, Mockito.times(1)).parseToFile(m1);
+        Mockito.verify(moduleParser, Mockito.times(1)).parseToFile(m2);
     }
 }

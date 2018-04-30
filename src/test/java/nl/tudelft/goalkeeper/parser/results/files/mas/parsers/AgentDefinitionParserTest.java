e45751059684551031943c2bfebc6061354d870f
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-30 18:51:54 +0200

    Add AgentDefinitionParser tests

---
+++ src/test/java/nl/tudelft/goalkeeper/parser/results/files/mas/parsers/AgentDefinitionParserTest.java
@@ -0,0 +1,19 @@
+package nl.tudelft.goalkeeper.parser.results.files.mas.parsers;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Test class for the AgentDefinitionParser class.
+ */
+class AgentDefinitionParserTest {
+
+    private AgentDefinitionParser parser;
+
+    /**
+     * Sets up the testing environment before each test.
+     */
+    @BeforeEach
+    void setup() {
+        parser = new AgentDefinitionParser();
+    }
+}

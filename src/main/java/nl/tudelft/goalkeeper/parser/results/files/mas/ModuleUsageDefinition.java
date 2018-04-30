25873d1316c76954ae04e628445f55a9a4162e5e
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-30 19:17:24 +0200

    Add ModuleUsageDefinition class

---
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas/ModuleUsageDefinition.java
@@ -0,0 +1,25 @@
+package nl.tudelft.goalkeeper.parser.results.files.mas;
+
+import lombok.Getter;
+import lombok.Setter;
+import nl.tudelft.goalkeeper.checking.violations.source.Source;
+import nl.tudelft.goalkeeper.parser.results.files.module.ModuleFile;
+import nl.tudelft.goalkeeper.parser.results.parts.Sourceable;
+
+/**
+ * Class representing module use cases.
+ */
+public final class ModuleUsageDefinition implements Sourceable {
+
+    @Getter private String target;
+    @Getter @Setter private ModuleFile module;
+    @Getter @Setter private Source source;
+
+    /**
+     * Creates a new ModuleUsageDefinitionInstance.
+     * @param target
+     */
+    public ModuleUsageDefinition(String target) {
+        this.target = target;
+    }
+}

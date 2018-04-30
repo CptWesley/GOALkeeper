9540965d19a71d6330be0cf6544ab7f8f8c8648f
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-30 18:22:23 +0200

    Add agent definition parsing to mas parsing

--- src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas/parsers/MasParser.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas/parsers/MasParser.java
@@ -1,6 +1,8 @@
 package nl.tudelft.goalkeeper.parser.results.files.mas.parsers;
 
 import languageTools.program.mas.MASProgram;
+import lombok.Getter;
+import lombok.Setter;
 import nl.tudelft.goalkeeper.parser.results.files.mas.Mas;
 
 import java.io.IOException;
@@ -10,6 +12,15 @@ import java.io.IOException;
  */
 public final class MasParser {
 
+    @Getter @Setter private AgentDefinitionParser agentDefinitionParser;
+
+    /**
+     * Creates a new instance of the MasParser class.
+     */
+    public MasParser() {
+        agentDefinitionParser = new AgentDefinitionParser();
+    }
+
     /**
      * Parses a GOAL MASProgram to a Mas.
      * @param program Program to parse.
@@ -18,6 +29,11 @@ public final class MasParser {
      */
     public Mas parse(MASProgram program) throws IOException {
         Mas mas = new Mas(program.getSourceFile().toString());
+
+        for (String name : program.getAgentNames()) {
+            mas.addAgentDefinition(agentDefinitionParser.parse(program.getAgentDefinition(name)));
+        }
+        
         return mas;
     }
 }

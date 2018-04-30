d63c545544eff35330de3dbc6f0b5cb66b6c3f65
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-30 15:22:27 +0200

    Add basic MasParser class

---
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas/parsers/MasParser.java
@@ -0,0 +1,19 @@
+package nl.tudelft.goalkeeper.parser.results.files.mas.parsers;
+
+import languageTools.program.mas.MASProgram;
+import nl.tudelft.goalkeeper.parser.results.files.mas.Mas;
+
+/**
+ * Class which parses mas2g files.
+ */
+public final class MasParser {
+
+    /**
+     * Parses a GOAL MASProgram to a Mas.
+     * @param program Program to parse.
+     * @return GOALKeeper variant of the MASProgram.
+     */
+    public Mas parse(MASProgram program) {
+        return null;
+    }
+}

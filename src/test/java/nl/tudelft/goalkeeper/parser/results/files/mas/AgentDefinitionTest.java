ed318bf008e23766a27214a5c656de0c5bbd886e
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-30 18:47:15 +0200

    Add AgentDefinitionTest base

---
+++ src/test/java/nl/tudelft/goalkeeper/parser/results/files/mas/AgentDefinitionTest.java
@@ -0,0 +1,19 @@
+package nl.tudelft.goalkeeper.parser.results.files.mas;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Test class for the AgentDefinition class.
+ */
+class AgentDefinitionTest {
+
+    private AgentDefinition definition;
+
+    /**
+     * Sets up the testing environment before each test.
+     */
+    @BeforeEach
+    void setup() {
+        definition = new AgentDefinition();
+    }
+}

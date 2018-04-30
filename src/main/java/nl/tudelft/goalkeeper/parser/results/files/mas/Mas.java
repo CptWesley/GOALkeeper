573fac98a5933202edd48630ff78d27e4d2026be
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-30 18:17:07 +0200

    Add agent definitions to the mas

--- src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas/Mas.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas/Mas.java
@@ -3,11 +3,17 @@ package nl.tudelft.goalkeeper.parser.results.files.mas;
 import nl.tudelft.goalkeeper.parser.results.files.File;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Class representing a MAS.
  */
 public final class Mas extends File {
+
+    private List<AgentDefinition> definitions;
+
     /**
      * Creates a new File instance.
      * @param fileName fileName of the file.
@@ -15,5 +21,23 @@ public final class Mas extends File {
      */
     protected Mas(String fileName) throws IOException {
         super(fileName);
+        definitions = new ArrayList<>();
     }
+
+    /**
+     * Gets all agent definitions in this mas.
+     * @return All agent definitions in this mas.
+     */
+    public List<AgentDefinition> getAgentDefinitions() {
+        return Collections.unmodifiableList(definitions);
+    }
+
+    /**
+     * Adds an agent definition to this mas.
+     * @param definition The definition to add.
+     */
+    public void addAgentDefinition(AgentDefinition definition) {
+        definitions.add(definition);
+    }
+
 }

7aa115afcbe3a04ac4e7eda300b2557b0bd179f3
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-05-01 08:07:44 +0200

    Add usage definitions to the agent definition

--- src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas/AgentDefinition.java
+++ src/main/java/nl/tudelft/goalkeeper/parser/results/files/mas/AgentDefinition.java
@@ -12,6 +12,8 @@ public final class AgentDefinition implements Sourceable {
 
     @Getter @Setter private Source source;
     @Getter private String name;
+    @Getter @Setter private ModuleUsageDefinition initModule,
+            mainModule, eventModule, shutDownModule;
 
     /**
      * Creates a new AgentDefinitionInstance.
@@ -19,5 +21,41 @@ public final class AgentDefinition implements Sourceable {
      */
     public AgentDefinition(String name) {
         this.name = name;
+        this.initModule = null;
+        this.mainModule = null;
+        this.eventModule = null;
+        this.shutDownModule = null;
+    }
+
+    /**
+     * Checks if this agent definition has an init module specified.
+     * @return True if it has an init module, false otherwise.
+     */
+    public boolean hasInitModule() {
+        return initModule != null;
+    }
+
+    /**
+     * Checks if this agent definition has a main module specified.
+     * @return True if it has a main module, false otherwise.
+     */
+    public boolean hasMainModule() {
+        return mainModule != null;
+    }
+
+    /**
+     * Checks if this agent definition has a shutdown module specified.
+     * @return True if it has a shutdown module, false otherwise.
+     */
+    public boolean hasShutdownModule() {
+        return shutDownModule != null;
+    }
+
+    /**
+     * Checks if this agent definition has an event module specified.
+     * @return True if it has an event module, false otherwise.
+     */
+    public boolean hasEventModule() {
+        return eventModule != null;
     }
 }
--- src/test/java/nl/tudelft/goalkeeper/parser/results/files/mas/AgentDefinitionTest.java
+++ src/test/java/nl/tudelft/goalkeeper/parser/results/files/mas/AgentDefinitionTest.java
@@ -2,6 +2,7 @@ package nl.tudelft.goalkeeper.parser.results.files.mas;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +28,53 @@ class AgentDefinitionTest {
     @Test
     void nameTest() {
        assertThat(definition.getName()).isEqualTo(NAME);
+    }
+
+    /**
+     * Checks that having an init module is correctly reflected in the 'has' method.
+     */
+    @Test
+    void initModuleTest() {
+        ModuleUsageDefinition mud = Mockito.mock(ModuleUsageDefinition.class);
+        assertThat(definition.hasInitModule()).isFalse();
+        definition.setInitModule(mud);
+        assertThat(definition.getInitModule()).isSameAs(mud);
+        assertThat(definition.hasInitModule()).isTrue();
+    }
+
+    /**
+     * Checks that having a main module is correctly reflected in the 'has' method.
+     */
+    @Test
+    void mainModuleTest() {
+        ModuleUsageDefinition mud = Mockito.mock(ModuleUsageDefinition.class);
+        assertThat(definition.hasMainModule()).isFalse();
+        definition.setMainModule(mud);
+        assertThat(definition.getMainModule()).isSameAs(mud);
+        assertThat(definition.hasMainModule()).isTrue();
+    }
+
+    /**
+     * Checks that having an event module is correctly reflected in the 'has' method.
+     */
+    @Test
+    void eventModuleTest() {
+        ModuleUsageDefinition mud = Mockito.mock(ModuleUsageDefinition.class);
+        assertThat(definition.hasEventModule()).isFalse();
+        definition.setEventModule(mud);
+        assertThat(definition.getEventModule()).isSameAs(mud);
+        assertThat(definition.hasEventModule()).isTrue();
+    }
+
+    /**
+     * Checks that having a shutdown module is correctly reflected in the 'has' method.
+     */
+    @Test
+    void shutdownModuleTest() {
+        ModuleUsageDefinition mud = Mockito.mock(ModuleUsageDefinition.class);
+        assertThat(definition.hasShutdownModule()).isFalse();
+        definition.setShutDownModule(mud);
+        assertThat(definition.getShutDownModule()).isSameAs(mud);
+        assertThat(definition.hasShutdownModule()).isTrue();
     }
 }

a58b8b85dab066778aec80c3286c0144c0eba242
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-05-22 20:27:30 +0200

    Fix violation stuff

--- src/main/java/nl/tudelft/goalkeeper/checking/CheckerRunner.java
+++ src/main/java/nl/tudelft/goalkeeper/checking/CheckerRunner.java
@@ -24,23 +24,24 @@ public final class CheckerRunner {
      * @return Collection of all violations.
      */
     public Collection<Violation> run(String masPath, RuleSet ruleSet) {
-
+        List<Violation> violations = new ArrayList<>();
         Parser parser = new Parser(masPath);
         ParseResult parseResult = parser.parse();
-
-        List<Violation> violations = new ArrayList<>();
-        Set<Class<?>> classes
-                = new Reflections("nl.tudelft.goalkeeper.checking.checkers")
-                .getTypesAnnotatedWith(Checker.class);
-        for (Class c : classes) {
-            CheckerInterface checker;
-            try {
-                checker = (CheckerInterface) c.newInstance();
-            } catch (Exception e) {
-                e.printStackTrace();
-                continue;
+        violations.addAll(parseResult.getViolations());
+        if (parseResult.isSuccessful()) {
+            Set<Class<?>> classes
+                    = new Reflections("nl.tudelft.goalkeeper.checking.checkers")
+                    .getTypesAnnotatedWith(Checker.class);
+            for (Class c : classes) {
+                CheckerInterface checker;
+                try {
+                    checker = (CheckerInterface) c.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    continue;
+                }
+                violations.addAll(checker.run(parseResult, ruleSet));
             }
-            violations.addAll(checker.run(parseResult, ruleSet));
         }
         return violations;
     }
